6c8c9bbebdc5fa5f6c7ab30aefc23fe1dc584a7e
sam <sparks>
2021-07-24 12:20:38 -0700

    portfolio setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Tesimonials />
+      <Contact />
+    </main>
+  )
+}
